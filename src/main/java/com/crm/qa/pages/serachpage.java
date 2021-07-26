a6d946b7444349577aead1f469bc7167a0ac8854
anchal <anchalkaushal95@gmail.com>
2021-07-26 10:49:26 +0530

    adding minor change in searchpage

--- src/main/java/com/crm/qa/pages/serachpage.java
+++ src/main/java/com/crm/qa/pages/serachpage.java
@@ -3,6 +3,8 @@ package com.crm.qa.pages;
 public class serachpage {
 	public void prints(){
 		System.out.println("search page");
+		// this is added comment
+		System.out.println("search page");
 	}
 
 }
